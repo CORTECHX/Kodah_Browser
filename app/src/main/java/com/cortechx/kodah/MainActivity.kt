99672facbd1f37a2b4c834f3321622ca5e109ac9
Shawn Grant <shawngrant333@gmail.com>
2018-06-06 23:02:47 -0400

    added buttons

--- app/src/main/java/com/cortechx/kodah/MainActivity.kt
+++ app/src/main/java/com/cortechx/kodah/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.cortechx.kodah
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
+import android.text.Editable
 import android.view.MenuItem
+import android.view.View
+import android.webkit.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -19,8 +25,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+           //BRING UP CONTROLS
+            if(controls.visibility == View.INVISIBLE) {
+                controls.visibility = View.VISIBLE
+            }else{
+                controls.visibility = View.INVISIBLE
+            }
+        }
+        //BUTTONS
+        btn_back.setOnClickListener { view ->
+            webview.goBack()
+        }
+        btn_forward.setOnClickListener { view ->
+            webview.goForward()
+        }
+        btn_back.setOnClickListener { view ->
+            webview.reload()
         }
 
         val toggle = ActionBarDrawerToggle(
@@ -29,12 +49,68 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        //SETUP THE WEBVIEW
+        webview.settings.javaScriptEnabled = true
+        webview.settings.loadWithOverviewMode = true
+        webview.settings.useWideViewPort = true
+        webview.webViewClient = object : WebViewClient() {
+            val loadBar = Snackbar.make(webview, "LOADING...", Snackbar.LENGTH_INDEFINITE)
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                webview.loadUrl(request!!.url.toString())
+                return true
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                loadBar.setAction("STOP", {
+                    webview.stopLoading()
+                    loadBar.dismiss()
+                }).show()
+
+                UrlBar.setText(url)
+                super.onPageStarted(view, url, favicon)
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                loadBar.dismiss()
+                super.onPageFinished(view, url)
+            }
+
+            override fun onReceivedError(view: WebView?, request: WebResourceRequest?, error: WebResourceError?) {
+                Snackbar.make(webview, "ERROR LOADING PAGE", Snackbar.LENGTH_SHORT).show()
+                super.onReceivedError(view, request, error)
+            }
+        }
+        webview.webChromeClient = object :WebChromeClient(){
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+                return true
+            }
+
+            override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                AlertDialog.Builder(this@MainActivity).setMessage(message).setTitle(url+" Says:").show()
+                return true
+            }
+
+            override fun onReceivedIcon(view: WebView?, icon: Bitmap?) {
+                siteIcon.setImageBitmap(icon)
+                super.onReceivedIcon(view, icon)
+            }
+        }
+
+        webview.loadUrl("http://google.com")
+
     }
+
 
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
+        }
+        else if (webview.canGoBack()){
+            webview.goBack()
+        }
+        else {
             super.onBackPressed()
         }
     }
@@ -42,23 +118,19 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
 
+            //dev sites
+            R.id.nav_stack_overflow -> {
+                webview.loadUrl("http://stackoverflow.com")
             }
-            R.id.nav_slideshow -> {
-
+            R.id.nav_w3 -> {
+                webview.loadUrl("http://w3schools.com")
             }
-            R.id.nav_manage -> {
-
+            R.id.nav_android_devs -> {
+                webview.loadUrl("http://developer.android.com")
             }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
+            R.id.nav_fb_devs -> {
+                webview.loadUrl("http://developer.facebook.com")
             }
         }
 
